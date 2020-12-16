f41913e60a88d620f84c0a2ae072e687c25d8e77
Suzette Sousa <sousa.suzette.pro@gmail.com>
2020-12-16 14:52:30 +0100

    sort movies by rating

--- src/components/CardsList.js
+++ src/components/CardsList.js
@@ -3,9 +3,12 @@ import Card from './Card';
 
 const CardsList = (props) => {
   const numbers = props.movies;
-  const listItems = numbers.map((number) =>
-    <Card key={number.id.toString()} movie={number} />
-  );
+  const listItems = 
+    numbers
+    .sort((lessRated, moreRated) => moreRated.rating - lessRated.rating)
+    .map((number) =>
+      <Card key={number.id.toString()} movie={number} />
+    );
   return (
     <div className="container mx-auto px-4">
       <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">

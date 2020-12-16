f0a06237cc185f0d6afe35e9b5c217cac24cc6c5
Suzette Sousa <sousa.suzette.pro@gmail.com>
2020-12-16 14:19:24 +0100

    add 3 more movies data

--- src/App.js
+++ src/App.js
@@ -29,6 +29,36 @@ const movies = [
     description: "Une maison de rêve, un pavillon bourgeois discrètement cossu dissimule dans une banlieue résidentielle, c'est ici que résident Lester Burnhamm, sa femme Carolyn et leur fille Jane. L'agitation du monde et sa violence semblent bien loin ici. Mais derrière cette respectable façade se tisse une étrange et grinçante tragi-comédie familiale ou désirs inavoués, frustrations et violences refoulées conduiront inexorablement un homme vers la mort.",
     image: "https://brozkinos.com/wp-content/uploads/2014/06/American-Beauty-Movie-Poster-e1464333773209.jpg",
     rating: 4.2
+  },
+  {
+    id: 4,
+    title: "Blood diamond",
+    subtitle: "Aventure | Drame | Thriller",
+    description: "Alors qu'il purge une peine de prison pour ses trafics, Archer rencontre Solomon Vandy, un pêcheur d'origine Mende. Arraché à sa famille et forcé de travailler dans les mines diamantifères, ce dernier a trouvé - et caché - un diamant rose extrêmement rare. Accompagnés de Maddy Bowen, une journaliste idéaliste, les deux hommes s'embarquent pour un dangereux voyage en territoire rebelle pour récupérer le fameux caillou. Un voyage qui pourrait bien sauver la famille de Salomon et donner à Archer la seconde chance qu'il n'espérait plus.",
+    image: "https://fr.web.img6.acsta.net/medias/nmedia/18/63/12/34/18711805.jpg",
+    rating: 4.1
+  },
+  {
+    id: 5,
+    title: "Wonder Woman",
+    subtitle: "Action | Aventure | Fantastique",
+    description: "Alors qu'il purge une peine de prison pour ses trafics, Archer rencontre Solomon Vandy, un pêcheur d'origine Mende. Arraché à sa famille et forcé de travailler dans les mines diamantifères, ce dernier a trouvé - et caché - un diamant rose extrêmement rare. Accompagnés de Maddy Bowen, une journaliste idéaliste, les deux hommes s'embarquent pour un dangereux voyage en territoire rebelle pour récupérer le fameux caillou. Un voyage qui pourrait bien sauver la famille de Salomon et donner à Archer la seconde chance qu'il n'espérait plus.",
+    image: "https://images-na.ssl-images-amazon.com/images/I/713piRVSmzL._AC_SY606_.jpg",
+    rating: 3.8
+  },
+  {
+    id: 6,
+    title: "Love Actually",
+    subtitle: "Romance | Comédie",
+    description: `L'amour est partout, imprévisible, inexplicable, insurmontable. Il frappe quand il veut et souvent, ça fait pas mal de dégâts...
+    Pour le nouveau Premier Ministre britannique, il va prendre la jolie forme d'une jeune collaboratrice.
+    Pour l'écrivain au coeur brisé parti se réfugier dans le sud de la France, il surgira d'un lac.
+    Il s'éloigne de cette femme qui, installée dans une vie de couple ronronnante, suspecte soudain son mari de songer à une autre.
+    Il se cache derrière les faux-semblants de ce meilleur ami qui aurait bien voulu être autre chose que le témoin du mariage de celle qu'il aime.
+    Pour ce veuf et son beau-fils, pour cette jeune femme qui adore son collègue, l'amour est l'enjeu, le but, mais également la source d'innombrables complications.
+    En cette veille de Noël à Londres, ces vies et ces amours vont se croiser, se frôler et se confronter...`,
+    image: "https://fr.web.img6.acsta.net/medias/nmedia/18/35/15/06/18364463.jpg",
+    rating: 3.9
   }
 ];
 

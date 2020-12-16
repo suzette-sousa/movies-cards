1adfd2052757f8a9dcca9f59fd8d8d2cd4e1c845
Suzette Sousa <sousa.suzette.pro@gmail.com>
2020-12-16 15:45:42 +0100

    readjust header styles with infos rating

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,33 @@
+import ReactStars from "react-rating-stars-component";
 import React from 'react';
 
 function Header() {
   return (
     <div className="sticky top-0 mb-4 bg-white shadow-sm">
-      <header className="flex justify-center items-center container mx-auto p-4 text-center">
-        <svg className="inline-block h-8 w-8 text-gray-200" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z"/></svg>
-        <h1 className="inline-block mx-2 text-4xl uppercase tracking-wider font-extralight">Classement de films</h1>
-        <svg className="inline-block h-8 w-8 text-gray-200" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z"/></svg>
+      <header className="flex flex-wrap justify-between items-center container mx-auto p-4 text-center">
+        <h1 className="text-4xl uppercase tracking-wider font-extralight">Classement de films</h1>
+        <div className="flex flex-wrap items-center">
+          <span className="flex flex-wrap items-center mr-4">
+            <span className="inline-block mr-2">Du mieux noté</span>
+            <ReactStars
+              count={5}
+              value={5}
+              size={28}
+              edit={false}
+              activeColor="#ffd700"
+            />
+          </span>
+          <span className="flex flex-wrap items-center">
+            <span className="inline-block mr-2">au moins bien noté</span>
+            <ReactStars
+              count={5}
+              value={0}
+              size={28}
+              edit={false}
+              activeColor="#ffd700"
+            />
+          </span>
+        </div>
       </header>
     </div>
   )

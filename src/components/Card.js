f0201f68c8c2983fb4d8946a715c233ac8109ab2
Suzette Sousa <sousa.suzette.pro@gmail.com>
2020-12-16 13:30:43 +0100

    add stars rating component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,3 +1,4 @@
+import ReactStars from "react-rating-stars-component";
 import React from 'react';
 
 const Card = (props) => {
@@ -11,7 +12,17 @@ const Card = (props) => {
         <h3 className="font-bold mb-2">Synopsis :</h3>
         <p>{movie.description}</p>
       </div>
-      <p className="p-4 border-t">{movie.rating}<svg class="block h-8 w-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z"/></svg></p>
+      <div className="flex items-center p-4 border-t">
+        <ReactStars
+          count={5}
+          value={movie.rating}
+          size={28}
+          edit={false}
+          isHalf={true}
+          activeColor="#ffd700"
+        />
+        <span className="inline-block ml-2 leading-4 text-2xl">{movie.rating}</span>
+      </div>
     </li>
   );
 }
